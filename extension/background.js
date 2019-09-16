8f489f7f5cf2777650c7fe42fa18b10f493dbe95
simon <simonlhopkins@gmail.com>
2019-09-16 12:48:44 -0400

    this will all be discarded

--- extension/background.js
+++ extension/background.js
@@ -24,20 +24,31 @@ chrome.storage.sync.get(['game'], function(result) {
 chrome.runtime.onMessage.addListener(function(request, sender) {
 	console.log(request)
 	//if they want to just view the game data
-    if (request.message === "requestGameData"){
+    if (request.message === "getGame"){
     	console.log("requested from: ");
     	console.log(sender);
     	//have to set it in the callback to ensure that the value loads in time.
     	chrome.storage.sync.get(['game'], function(result) {
-    		chrome.tabs.sendMessage(sender.tab.id, {message: "requestGameData", data: result}, function(){
+    		chrome.tabs.sendMessage(sender.tab.id, {message: "getGame", data: result}, function(){
 
     		});
 		});
     }
 
-    if(request.message === "setGameData"){
-    	console.log("setGameData");
+    if(request.message === "setGame"){
+    	console.log("setGame");
     	chrome.storage.sync.set({'game': request.data}, function(result) {
+			console.log(request.data);
+		});
+    }
+
+    if(request.message === "clearGame"){
+    	console.log("clearGame");
+
+    	var _game = new Object();
+		_game.times = [];
+
+    	chrome.storage.sync.set({'game': _game}, function(result) {
 			console.log(request.data);
 		});
     }
--- extension/scripts/simonTest.js
+++ extension/scripts/simonTest.js
@@ -6,30 +6,68 @@ $(document).ready(function(){
 	
 
 	$(document).mousedown(function(){
-		chrome.runtime.sendMessage({message: "requestGameData"},function(){
-			
+		console.log("mouse down");
+		updateGame(function(data){
+			// var _game = data;
+			// console.log(_game);
+			console.log("updatedGame");
+		});
+	});
+
+	$(document).dblclick(function(){
+		chrome.runtime.sendMessage({message: "clearGame"},function(){
+			return Promise.resolve("Dummy response to keep the console quiet");
 		});
 	});
 });
 
 
 
+function updateGame(callback){
 
+	chrome.runtime.sendMessage({message: "getGame"},function(){
+		return Promise.resolve("Dummy response to keep the console quiet");
+	});
+
+	
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
 
-    if (request.message === "requestGameData"){
-    	console.log("client recieved game data")
+    if (request.message === "getGame"){
+
     	var newData = request.data.game;
-    	console.log("recieved data");
     	var d = new Date();
 		var n = d.getTime();
 		console.log(newData);
-    	newData.times.push(n);
-    	console.log("setting new game data...");
-    	chrome.runtime.sendMessage({message: "setGameData", data: newData},function(){});
+		newData.times.push(n);
+    	
+
+    	chrome.runtime.sendMessage({message: "setGame", data: newData},function(){
+
+    	});
+
+    	
     }
+
+    return Promise.resolve("Dummy response to keep the console quiet");
+
 });
+
+// chrome.runtime.onMessage.addListener(function(request, sender) {
+
+//     if (request.message === "requestGameData"){
+//     	console.log("client recieved game data")
+//     	var newData = request.data.game;
+//     	console.log("recieved data");
+//     	var d = new Date();
+// 		var n = d.getTime();
+// 		console.log(newData);
+//     	newData.times.push(n);
+//     	console.log("setting new game data...");
+//     	chrome.runtime.sendMessage({message: "setGameData", data: newData},function(){});
+//     }
+// });
 
 
 // chrome.runtime.onMessage.addListener(){
